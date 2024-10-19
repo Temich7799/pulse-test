29e510e56b5744b33087f1c6d68023c457b44e21
Temich7799 <artemgl9999@gmail.com>
2024-10-19 13:24:06 +0300

    highlighting logic

--- src/components/SortedList.jsx
+++ src/components/SortedList.jsx
@@ -14,13 +14,14 @@ import { sortArray } from '../utils';
 
 const SortedList = ({
   items = [],
-  label,
+
   onClick,
   onDoubleClick,
   onMultipleClick,
   placeholder,
 }) => {
   const [search, setSearch] = useState('');
+  const [selectedItems, setSelectedItems] = useState([]);
 
   const { current } = useRef({ isCtrlDown: false });
 
@@ -28,8 +29,10 @@ const SortedList = ({
     (e, item) => {
       if (onMultipleClick && current.isCtrlDown) {
         onMultipleClick(e, item);
+        setSelectedItems((prev) => [...prev, item]);
       } else if (onClick) {
         onClick(e, item);
+        setSelectedItems([item]);
       }
     },
     [onClick, onMultipleClick, current.isCtrlDown]
@@ -75,56 +78,48 @@ const SortedList = ({
   }, [onMultipleClick, current]);
 
   return (
-    <Paper sx={{ border: '2px solid #000', padding: 2 }}>
-      <Stack spacing={2}>
-        <Typography
-          component="h3"
-          sx={{ borderBottom: '2px solid #000', fontWeight: 'bold' }}
-          variant="h6"
-        >
-          {label}
-        </Typography>
-        <TextField
-          fullWidth
-          sx={{
-            '& .MuiOutlinedInput-root': {
-              '& fieldset': {
-                border: 'none',
-              },
+    <>
+      <TextField
+        fullWidth
+        sx={{
+          '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+              border: 'none',
             },
-            border: '2px solid #000',
-            borderRadius: 0,
-          }}
-          placeholder={placeholder}
-          value={search}
-          variant="outlined"
-          onChange={handleSearch}
-        />
-        <List sx={{ border: '2px solid #000', height: '150px', overflowY: 'scroll', padding: 0 }}>
-          {filteredItems.map((item) => (
-            <ListItemButton
-              sx={{
-                '&:hover': {
-                  backgroundColor: '#f0f0f0',
-                },
-                border: 'none',
-                borderBottom: '1px solid #000',
-                borderRadius: 0,
-                padding: '10px 16px',
-              }}
-              key={item.id}
-              onClick={(e) => onClickHandler(e, item)}
-              onDoubleClick={(e) => onDoubleClickHandler(e, item)}
-            >
-              <ListItemText
-                primary={item.name}
-                sx={{ textAlign: 'center' }}
-              />
-            </ListItemButton>
-          ))}
-        </List>
-      </Stack>
-    </Paper>
+          },
+          border: '2px solid #000',
+          borderRadius: 0,
+        }}
+        placeholder={placeholder}
+        value={search}
+        variant="outlined"
+        onChange={handleSearch}
+      />
+      <List sx={{ border: '2px solid #000', height: '150px', overflowY: 'scroll', padding: 0 }}>
+        {filteredItems.map((item) => (
+          <ListItemButton
+            sx={{
+              '&:hover': {
+                backgroundColor: '#f0f0f0',
+              },
+              backgroundColor: selectedItems.includes(item) ? '#d1eaff' : 'inherit',
+              border: 'none',
+              borderBottom: '1px solid #000',
+              borderRadius: 0,
+              padding: '10px 16px',
+            }}
+            key={item.id}
+            onClick={(e) => onClickHandler(e, item)}
+            onDoubleClick={(e) => onDoubleClickHandler(e, item)}
+          >
+            <ListItemText
+              primary={item.name}
+              sx={{ textAlign: 'center' }}
+            />
+          </ListItemButton>
+        ))}
+      </List>
+    </>
   );
 };
 
@@ -139,14 +134,25 @@ const WrappedSortedList = ({
   return (
     <Paper>
       <Stack spacing={1}>
-        <SortedList
-          items={items}
-          label={label}
-          placeholder={placeholder}
-          onClick={onClick}
-          onDoubleClick={onDoubleClick}
-          onMultipleClick={onMultipleClick}
-        />
+        <Paper sx={{ border: '2px solid #000', padding: 2 }}>
+          <Stack spacing={2}>
+            <Typography
+              component="h3"
+              sx={{ borderBottom: '2px solid #000', fontWeight: 'bold' }}
+              variant="h6"
+            >
+              {label}
+            </Typography>
+            <SortedList
+              items={items}
+              label={label}
+              placeholder={placeholder}
+              onClick={onClick}
+              onDoubleClick={onDoubleClick}
+              onMultipleClick={onMultipleClick}
+            />
+          </Stack>
+        </Paper>
       </Stack>
     </Paper>
   );
